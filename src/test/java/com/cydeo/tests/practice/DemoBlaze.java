1ee257a89d410fc1b59ff449041e81e59d1d3d19
Mehmet <mehmetnobel26@gmail.com>
2022-06-05 11:40:11 +0300

    Demoblaze

--- src/test/java/com/cydeo/tests/practice/DemoBlaze.java
+++ src/test/java/com/cydeo/tests/practice/DemoBlaze.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.practice;
 
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
 import org.openqa.selenium.*;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -34,7 +35,7 @@ public class DemoBlaze {
 
         WebElement sony_vaio_i5Button = driver.findElement(By.xpath("//img[@src='imgs/sony_vaio_5.jpg']/..//img"));
 
-        BrowserUtils.sleep(1);
+        BrowserUtils.sleep(2);
 
         sony_vaio_i5Button.click();
 
@@ -42,11 +43,71 @@ public class DemoBlaze {
 
         add_to_cart.click();
 
-        //Alert alert=driver.switchTo().alert();
+        BrowserUtils.sleep(2);
+
+        Alert alert=driver.switchTo().alert();
 
         BrowserUtils.sleep(2);
 
-        //alert.accept();
+        alert.accept();
+
+        driver.findElement(By.partialLinkText("Home")).click();
+
+        BrowserUtils.sleep(1);
+
+        driver.findElement(By.partialLinkText("Laptops")).click();
+
+        driver.findElement(By.partialLinkText("Dell i7 8gb")).click();
+
+        BrowserUtils.sleep(2);
+
+        driver.findElement(By.partialLinkText("Add to cart")).click();
+
+        BrowserUtils.sleep(2);
+
+        Alert alert2=driver.switchTo().alert();
+
+        BrowserUtils.sleep(2);
+
+        alert2.accept();
+
+        BrowserUtils.sleep(2);
+
+        driver.findElement(By.partialLinkText("Cart")).click();
+
+        driver.findElement(By.xpath("//button[@class='btn btn-success']")).click();
+
+        Faker faker=new Faker();
+
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys(faker.name().firstName());
+
+        driver.findElement(By.cssSelector("#country")).sendKeys(faker.country().name());
+
+        driver.findElement(By.cssSelector("#city")).sendKeys(faker.address().city());
+
+        driver.findElement(By.cssSelector("#card")).sendKeys("1234567891234567");
+
+        driver.findElement(By.cssSelector("#month")).sendKeys("11");
+
+        driver.findElement(By.cssSelector("#year")).sendKeys("2022");
+
+        driver.findElement(By.xpath("//button[@onclick='purchaseOrder()']")).click();
+
+        String purchasedText = driver.findElement(By.xpath("//p[@style='display: block;']")).getText();
+
+        System.out.println("purchasedText = " + purchasedText);
+
+        System.out.println("*********************************");
+
+        for (String s : purchasedText.split("\n")) {
+
+            System.out.println("s = " + s);
+
+        }
+
+        int price=Integer.parseInt(purchasedText.split("\n")[1].split(" ")[1]);
+
+        System.out.println("price = " + price);
 
 
     }
